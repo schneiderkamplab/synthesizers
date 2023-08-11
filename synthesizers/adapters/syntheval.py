418c480d8aaf5c20cac8c09c6f920281718131a5
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2023-08-11 12:16:47 +0200

    more syntheval

--- synthesizers/adapters/syntheval.py
+++ synthesizers/adapters/syntheval.py
@@ -4,17 +4,24 @@ from syntheval import SynthEval
 from .base import Adapter
 
 class SynthEvalAdapter(Adapter):
-    def __init__(self, input_formats=(DataFrame,), target_col: str = None):
+    def __init__(self, input_formats=(DataFrame,), config: str = 'full', target_col: str = None):
         super(SynthEvalAdapter, self).__init__(input_formats)
+        self.config = config
         self.target_col = target_col
     def train_model(self, data):
         raise NotImplementedError("training not implemented for SynthEval")
     def generate_data(self, count, model):
         raise NotImplementedError("generation not implemented for SynthEval")
-    def evaluate_generated(self, original_data, generated_data, target_col: str = None):
+    def evaluate_generated(self, original_data, generated_data, config: str = 'full', target_col: str = None):
         original_data = self.ensure_input_format(original_data)
         generated_data = self.ensure_input_format(generated_data)
         evaluator = SynthEval(real=original_data)
+        config = self.config if config is None else config
         target_col = self.target_col if target_col is None else target_col
-        evaluator.full_eval(synthetic_data=generated_data, target_col=target_col)
+        if config == 'full':
+            evaluator.full_eval(synthetic_data=generated_data, target_col=target_col)
+        elif config == 'fast':
+            evaluator.fast_eval(synthetic_dataframe=generated_data, target=target_col)
+        else:
+            raise ValueError(f"unknown config {config} -- use either 'full' or 'fast'")
         return evaluator.res_dict
