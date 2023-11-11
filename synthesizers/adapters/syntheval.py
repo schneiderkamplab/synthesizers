ec240d1d086441778c577250e6a0deacc9d3e4c4
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2023-11-11 09:09:49 +0100

    toward syntheval 1.3

--- synthesizers/adapters/syntheval.py
+++ synthesizers/adapters/syntheval.py
@@ -4,23 +4,18 @@ from syntheval import SynthEval
 from .base import Adapter
 
 class SynthEvalAdapter(Adapter):
-    def __init__(self, input_formats=(DataFrame,), config: str = 'full', unique_thresh: int = 10):
+    def __init__(self, input_formats=(DataFrame,), presets_file: str = 'full_eval', unique_threshold: int = 10):
         super(SynthEvalAdapter, self).__init__(input_formats)
-        self.config = config
-        self.unique_thresh = unique_thresh
+        self.config = presets_file
+        self.unique_thresh = unique_threshold
     def train_model(self, data):
         raise NotImplementedError("training not implemented for SynthEval")
     def generate_data(self, count, model):
         raise NotImplementedError("generation not implemented for SynthEval")
-    def evaluate_generated(self, original_data, generated_data, config: str = None, hold_out = None, cat_cols: str = None, unique_thresh: int = 10, target_col: str = None):
+    def evaluate_generated(self, original_data, generated_data, config: str = None, hold_out = None, cat_cols: str = None, unique_threshold: int = 10, target_col: str = None):
         original_data = self.ensure_input_format(original_data)
         generated_data = self.ensure_input_format(generated_data)
-        evaluator = SynthEval(real=original_data, hold_out=hold_out, cat_cols=cat_cols, unique_thresh=unique_thresh)
+        evaluator = SynthEval(real=original_data, hold_out=hold_out, cat_cols=cat_cols, unique_threshold=unique_threshold)
         config = self.config if config is None else config
-        if config == 'full':
-            evaluator.full_eval(synthetic_data=generated_data, target_col=target_col)
-        elif config == 'fast':
-            evaluator.fast_eval(synthetic_dataframe=generated_data, target=target_col)
-        else:
-            raise ValueError(f"unknown config {config} -- use either 'full' or 'fast'")
+        evaluator.evaluate(synthetic_dataframe=generated_data, analysis_target_var=target_col, presets_file=config)
         return evaluator.res_dict
