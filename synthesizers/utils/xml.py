1a53ab5d0e7f086860e1210dd654491de51553e6
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2024-11-14 23:38:46 +0100

    full package list

--- synthesizers/utils/xml.py
+++ synthesizers/utils/xml.py
@@ -10,12 +10,6 @@ TYPE_TAG = "Type"
 PARAMETERS_TAG = "Parameters"
 PART_TAG = "Part"
 
-PACKAGES = (
-    'py-synthpop',
-    'synthcity',
-    'syntheval',
-)
-
 def unwrap_model_xml(model_type, xml_data):
     model_data = b""
     for i, xml_datum in enumerate(xml_data, start=1):
@@ -28,8 +22,11 @@ def unwrap_model_xml(model_type, xml_data):
                 if version.get("number") != platform.python_version():
                     print(f"Warning: model was trained with python {version.get('number')}, but current Python version is {platform.python_version()}", file=stderr)
             else:
-                if version.get("number") != importlib.metadata.version(package):
-                    print(f"Warning: model was trained with {package} {version.get('number')}, but current {package} version is {importlib.metadata.version(package)}", file=stderr)
+                try:
+                    if version.get("number") != importlib.metadata.version(package):
+                        print(f"Warning: model was trained with {package} {version.get('number')}, but current {package} version is {importlib.metadata.version(package)}", file=stderr)
+                except importlib.metadata.PackageNotFoundError:
+                    print(f"Warning: model was trained with {package} {version.get('number')}, but {package} is not installed", file=stderr)
             assert root.find(PART_TAG).get("number") == str(i) and root.find(PART_TAG).get("total") == str(len(xml_data))
         model_data += base64.b64decode(root.find(PARAMETERS_TAG).text)
     return model_data
@@ -43,8 +40,8 @@ def wrap_model_xml(model_type, model_data, part_size):
         model_type_elem = ET.SubElement(root, TYPE_TAG)
         model_type_elem.text = model_type
         ET.SubElement(root, VERSION_TAG, id="python", number=platform.python_version())
-        for PACKAGE in PACKAGES:
-            ET.SubElement(root, VERSION_TAG, id=PACKAGE, number=importlib.metadata.version(PACKAGE))
+        for dist in importlib.metadata.distributions():
+            ET.SubElement(root, VERSION_TAG, id=dist.metadata['Name'], number=dist.version)
         ET.SubElement(root, PART_TAG, number=str(i), total=str(num_parts))
         model_data_elem = ET.SubElement(root, PARAMETERS_TAG)
         model_data_elem.text = base64.b64encode(part).decode("utf-8")
